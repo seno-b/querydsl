a66732a63ee2cfa09d1baaa153a7a6b8396ae5fc
seno-b <bangshinho@gmail.com>
2021-09-04 19:13:13 +0900

    Feat: 2. 프로젝트 환경설정 Querydsl 설정과 검증

--- src/test/java/com/senob/querydsl/QuerydslApplicationTests.java
+++ src/test/java/com/senob/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,38 @@
 package com.senob.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.senob.querydsl.entity.Hello;
+import com.senob.querydsl.entity.QHello;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(em);
+
+		QHello qHello = QHello.hello;
+
+		Hello result = jpaQueryFactory
+				.selectFrom(qHello)
+				.fetchOne();
+
+		Assertions.assertThat(result).isEqualTo(hello);
+		Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
 	}
 
 }
