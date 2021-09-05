94af8650c700048ae4cad9a3f020ddeb28824692
seno-b <bangshinho@gmail.com>
2021-09-05 23:51:39 +0900

    Feat: 4. 기본 문법 조인 - on절

--- src/test/java/com/senob/querydsl/QuerydslBasicTest.java
+++ src/test/java/com/senob/querydsl/QuerydslBasicTest.java
@@ -272,4 +272,52 @@ public class QuerydslBasicTest {
                 .extracting("username")
                 .containsExactly("teamA", "teamB");
     }
+
+    /**
+     * 예) 회원과 팀을 조인하면서, 팀 이름이 teamA임 팀만 조인, 회원은 모두 조회
+     * JPQL :
+     *     select
+     *         member1,
+     *         team
+     *     from
+     *         Member member1
+     *     left join
+     *         member1.team as team with team.name = 'teamA'
+     */
+    @Test
+    public void join_on_filtering() {
+        List<Tuple> result = jpaQueryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(member.team, team)
+                .on(team.name.eq("teamA"))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    /**
+     * 연관관계 없는 조인
+     * 회원의 이름이 팀 이름과 같은 대상 조회
+     */
+
+    @Test
+    public void join_on_no_releation() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Tuple> result = jpaQueryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(team)
+                .on(team.name.eq(member.username))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
 }
