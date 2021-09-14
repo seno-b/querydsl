576fe46f2adfcda64adbbe97a406a1961c997fd1
seno-b <bangshinho@gmail.com>
2021-09-15 00:13:17 +0900

    Feat: 6. 실무 활용 - 순수 JPA와 QueryDsl 조회 API 컨트롤러 개발

---
+++ src/main/java/com/senob/querydsl/controller/MemberController.java
@@ -0,0 +1,25 @@
+package com.senob.querydsl.controller;
+
+import com.senob.querydsl.dto.MemberSearchCondition;
+import com.senob.querydsl.dto.MemberTeamDto;
+import com.senob.querydsl.repository.MemberJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class MemberController {
+
+    private final MemberJpaRepository memberJpaRepository;
+
+    @GetMapping("/v1/members")
+    public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
+        return memberJpaRepository.search(condition);
+    }
+}
+
