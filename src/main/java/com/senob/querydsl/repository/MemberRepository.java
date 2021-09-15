a428edeae9d70b64aad9a4eae7a48ce447d2a7e6
seno-b <bangshinho@gmail.com>
2021-09-15 23:11:41 +0900

    Feat: 6. 실무 활용 - 스프링 데이터 JPA와 Querydsl 스프링 데이터 JPA 리포지토리로 변경

---
+++ src/main/java/com/senob/querydsl/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.senob.querydsl.repository;
+
+import com.senob.querydsl.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByUsername(String username);
+}
---
+++ src/test/java/com/senob/querydsl/repository/MemberRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.senob.querydsl.repository;
+
+import com.senob.querydsl.entity.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+public class MemberRepositoryTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void basicTest() {
+        Member member = new Member("member1", 10);
+        memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(member.getId()).get();
+        Assertions.assertThat(findMember).isEqualTo(member);
+
+        List<Member> result1 = memberRepository.findAll();
+        Assertions.assertThat(result1).containsExactly(member);
+
+        List<Member> result2 = memberRepository.findByUsername("member1");
+        Assertions.assertThat(result2).containsExactly(member);
+    }
+}
