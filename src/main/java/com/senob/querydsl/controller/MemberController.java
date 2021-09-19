4fe79c354f056848605efdf2768d96308013af64
seno-b <bangshinho@gmail.com>
2021-09-19 22:47:36 +0900

    Feat: 6. 실무 활용 - 스프링 데이터 JPA와 Querydsl 스프링 데이터 페이징 활용3 - 컨트롤러 개발

--- src/main/java/com/senob/querydsl/controller/MemberController.java
+++ src/main/java/com/senob/querydsl/controller/MemberController.java
@@ -3,7 +3,10 @@ package com.senob.querydsl.controller;
 import com.senob.querydsl.dto.MemberSearchCondition;
 import com.senob.querydsl.dto.MemberTeamDto;
 import com.senob.querydsl.repository.MemberJpaRepository;
+import com.senob.querydsl.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +19,21 @@ import java.util.List;
 public class MemberController {
 
     private final MemberJpaRepository memberJpaRepository;
+    private final MemberRepository me,memberRepository;
 
     @GetMapping("/v1/members")
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
         return memberJpaRepository.search(condition);
+    }
+
+    @GetMapping("/v2/members")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageSimple(condition, pageable);
+    }
+
+    @GetMapping("/v3/members")
+    public Page<MemberTeamDto> searchMemberV3(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageCo(condition, pageable);
     }
 }
 
